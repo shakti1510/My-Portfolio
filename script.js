999a6938924ff72ae066d152ade004f7629b4577
shakti1510 <shashankmnshakti95288@gmail.com>
2024-01-20 20:36:17 +0530

    Present Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typeData = new Typed(".role", {
+      strings: [
+        "Coder",
+        "Software Developer",
+        "Full Stack Developer",
+        "Web Developer",
+      ],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 0,
+    });
+  });
+  function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_56f847q";
+    const templateID = "template_8z2cvzg";
+  
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!");
+      })
+      .catch((err) => console.log(err));
+  }
+  
