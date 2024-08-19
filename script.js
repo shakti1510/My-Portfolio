fb70648a0161372767708975c138414fb103d5d9
shakti1510 <shashankmnshakti95288@gmail.com>
2024-08-19 14:49:40 +0530

    Email response Addition

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var typeData = new Typed(".role", {
-      strings: [
-        "Coder",
-        "Software Developer",
-        "Full Stack Developer",
-        "Web Developer",
-      ],
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 80,
-      backDelay: 0,
-    });
+  var typeData = new Typed(".role", {
+    strings: [
+      "Coder",
+      "Software Developer",
+      "Full Stack Developer",
+      "Web Developer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 0,
   });
-  function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_56f847q";
-    const templateID = "template_8z2cvzg";
-  
-    emailjs
-      .send(serviceID, templateID, params)
-      .then((res) => {
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!");
-      })
-      .catch((err) => console.log(err));
-  }
-  
+});
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_02qdukl";
+  const templateID = "template_akmd2q5";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
